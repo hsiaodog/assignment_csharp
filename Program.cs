548441f1311abb809f1b355ba738f3b28a707252
hsiaodog <a37470207@gmail.com>
2021-09-21 23:44:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int select_func = 1;
+            four_assignment Assignment_4 = new four_assignment();
+            while (select_func >=1 & select_func <=4)
+            {
+                Console.Write("1 for factorial; 2 for prime number; 3 for leap; 4 for lcm: ");
+                select_func = Convert.ToInt32(Console.ReadLine());
+                if (select_func == 1)
+                {
+                    Console.Write("Find factorial of the number: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Assignment_4.factorial(a);
+                }
+                else if (select_func == 2)
+                {
+                    Console.Write("checking number is prime number or not: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Assignment_4.prime_num(a);
+                }
+
+                else if (select_func == 3)
+                {
+                    Console.Write("checking year is leap or not: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Assignment_4.leap_year(a);
+                }
+                else
+                {
+                    Console.WriteLine("Find LCM of two numbers: ");
+                    Console.Write("First number: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Second number: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    Assignment_4.lcm(a,b);
+                }
+            }
+        }
+    }
+}
---
+++ four_assignment.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class four_assignment
+    {
+        public void factorial(int num)
+        {
+            int ans = 1;
+            int iter = num;
+            int check_ = 1;
+            //Console.WriteLine("Hello World!");
+            while (iter != 0 && check_ == 1)
+            {
+                ans *= iter;
+                iter--;
+                if (ans > 2147483647)
+                {
+                    check_ = 0;
+                    Console.WriteLine("exceeding the maximum of int32");
+                }
+                else if (ans < -2147483648)
+                {
+                    check_ = 0;
+                    Console.WriteLine("exceeding the minimum of int32");
+                }
+            }
+            if (check_ == 1)
+            {
+                Console.WriteLine(ans);
+            }
+        }
+        public void leap_year(int year)
+        {
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("This is leap year.");
+            }
+            else
+            {
+                Console.WriteLine("This is not leap year.");
+            }
+        }
+        public void lcm(int a, int b)
+        {
+            int[] ans = { };
+            int max_ab;
+            int gcd =1;
+            int ans_;
+            if (a >= b)
+            {
+                max_ab = b;
+            }
+            else
+            {
+                max_ab = a;
+            }
+            for (int i=1; i<=max_ab;i++)
+            {
+                if (a%i==0 & b%i==0)
+                {
+                    gcd = i;
+                }
+            }
+            ans_ = a * b / gcd;
+            if (ans_ == 0)
+            {
+                Console.WriteLine($"The LCM of two numbers ({a},{b}) exceeds the space of int32.");
+            }
+            else
+            {
+                Console.WriteLine($"The LCM of two numbers ({a},{b}) is {ans_}.");
+            }
+            
+        }
+        public void prime_num (int a)
+        {
+            int ans_ = a;
+            for (int i = 2; i <= a; i++)
+            {
+                if (a%i == 0 & i!= a)
+                {
+                    ans_ = i;
+                    break;
+                }
+            }
+            if (ans_ == a)
+            {
+                Console.WriteLine($"{a} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{a} is not a prime number.");
+            }
+        }
+    }
+}
